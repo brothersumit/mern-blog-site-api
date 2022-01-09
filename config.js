396b799026099b660100e550bf5337d0630c8272
hisumitkumar <sumitkumar201sk@gmail.com>
2022-01-09 22:03:47 +0530

    some fixes

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    'mongoUrl' : 'mongodb+srv://adminblogsite:blogsitepass123@mern-blog-site.nobu1.mongodb.net/mernblogsitedb?retryWrites=true&w=majority',
-    'site_url' : 'http://localhost:5000'
+    'mongoUrl' : 'mongodb+srv://adminblogsite:blogsitepass123@mern-blog-site.nobu1.mongodb.net/mernblogsitedb?retryWrites=true&w=majority'
 }
